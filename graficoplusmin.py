a5a1159e96ce2aeabcb7e02eabe17a6cb19abf0f
Condor Basquete Brasil <condorbballbr@gmail.com>
2026-04-20 14:36:35 -0300

    Update graficoplusmin.py

--- graficoplusmin.py
+++ graficoplusmin.py
@@ -11,13 +11,12 @@ plt.title("Média Final de Plus/Minus")
 plt.xlabel("Jogadores")
 plt.ylabel("Plus/Minus")
 
+plt.ylim(0, 12)
+
 for i, v in enumerate(medias):
     plt.text(i, v + 0.2, f"{v}", ha='center')
 
 plt.show()
-
-
-
 
 
 
@@ -33,13 +32,15 @@ labels = [
 
 valores = [5.0, 3.0, 11.0, 0.0, 4.3, 2.0, 1.7]
 
-plt.figure(figsize=(10, 5))  # deixa mais largo pra não ficar apertado
+plt.figure(figsize=(10, 5))
 
 plt.bar(labels, valores)
 
 plt.title("Impacto das Combinações em Quadra")
 plt.xlabel("Formações")
 plt.ylabel("Plus/Minus")
+
+plt.ylim(0, 12)
 
 for i, v in enumerate(valores):
     plt.text(i, v + 0.2, f"{v}", ha='center')
