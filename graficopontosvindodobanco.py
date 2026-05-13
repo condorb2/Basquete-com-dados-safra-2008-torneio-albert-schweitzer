13ba7c198b07d00ed6f9fcf2846912848433562a
Condor Basquete Brasil <condorbballbr@gmail.com>
2026-05-13 16:15:07 -0300

    graficopontosvindodobanco.py

--- graficopontosvindodobanco.py
+++ graficopontosvindodobanco.py
@@ -7,22 +7,22 @@ jogadores = [
     "Gustavs Brutans",
     "Gabrielle Turconi",
     "Aleksa Stikovic",
-    "Caspar Vossenberg"
+    "Kenan Youdom"
 ]
 
-pontos = [9.1, 3.0, 4.3, 7.0, 4.8, 4.6, 11.2]
+pontos = [9.1, 3.0, 4.3, 7.0, 4.8, 4.6, 9.8]
 
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(11, 5))
 
 plt.bar(jogadores, pontos)
 
-plt.title("Pontos Vindo do Banco")
-plt.ylim(0, 13)  # Vai até 13 no eixo Y
+plt.title("Pontos do Banco: Edu Santos vs. média dos jogadores mais pontuaram vindo do banco contra o Brasil")
+plt.ylim(0, 10)
 
 for i, v in enumerate(pontos):
-    plt.text(i, v + 0.2, f"{v}", ha='center')
+    plt.text(i, v + 0.15, f"{v}", ha='center')
 
-plt.xticks(rotation=15)
+plt.xticks(rotation=10)
 plt.tight_layout()
 
 plt.show()
