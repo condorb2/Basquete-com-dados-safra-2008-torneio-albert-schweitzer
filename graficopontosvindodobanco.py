2f469cc68d2be0354b94d3392b6e29f841049069
Condor Basquete Brasil <condorbballbr@gmail.com>
2026-05-14 20:17:27 -0300

    Update graficopontosvindodobanco.py

--- graficopontosvindodobanco.py
+++ graficopontosvindodobanco.py
@@ -1,26 +1,26 @@
 import matplotlib.pyplot as plt
 
 jogadores = [
-    "Edu Santos",
-    "Hanzhi Tu",
-    "Hunter Sevil",
-    "Gustavs Brutans",
-    "Gabrielle Turconi",
-    "Aleksa Stikovic",
-    "Kenan Youdom"
+    "Pietro Melo",
+    "Yuxiu Chen",
+    "Arana Robertson",
+    "Raivo Kudrjavcevs",
+    "Federico Bottelli",
+    "Aleksej Ivkovic",
+    "Jamie Edoka"
 ]
 
-pontos = [9.1, 3.0, 4.3, 7.0, 4.8, 4.6, 9.8]
+medias = [20.0, 13.2, 9.9, 12.5, 20.1, 11.7, 17.8]
 
 plt.figure(figsize=(11, 5))
 
-plt.bar(jogadores, pontos)
+plt.bar(jogadores, medias)
 
-plt.title("Pontos do Banco: Edu Santos vs. média dos jogadores mais pontuaram vindo do banco contra o Brasil")
-plt.ylim(0, 10)
+plt.title("Armadores que Mais Combinaram Médias de Assistências e Pontos")
+plt.ylim(0, 11)
 
-for i, v in enumerate(pontos):
-    plt.text(i, v + 0.15, f"{v}", ha='center')
+for i, v in enumerate(medias):
+    plt.text(i, v + 0.2, f"{v}", ha='center')
 
 plt.xticks(rotation=10)
 plt.tight_layout()
